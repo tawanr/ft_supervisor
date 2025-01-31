2042746c4a33cd718073d235446fb3c2e958517c
tratanat <tawan.rtn@gmail.com>
2025-01-31 18:37:26 +0700

    Add job status

--- cmd/supervisord/job.go
+++ cmd/supervisord/job.go
@@ -8,35 +8,30 @@ import (
 	"text/scanner"
 )
 
-// ParseCommand parses a command string into a path and arguments
-// Returns command path and arguments
-func ParseCommand(command string) (string, []string) {
-	var s scanner.Scanner
-	s.Init(strings.NewReader(command))
-	args := []string{}
-	token := s.Scan()
-	for token != scanner.EOF {
-		args = append(args, s.TokenText())
-		token = s.Scan()
-	}
-	path, err := exec.LookPath(args[0])
-	if err != nil {
-		path = args[0]
-	}
-	return path, args[1:]
-}
-
 type Job struct {
 	config    *ConfigProgram
 	pid       int
 	cmd       *exec.Cmd
 	isRunning bool
+	status    JobStatus
 }
+
+type JobStatus string
+
+const (
+	STATUS_RUNNING  JobStatus = "RUNNING"
+	STATUS_STOPPED  JobStatus = "STOPPED"
+	STATUS_EXITED   JobStatus = "EXITED"
+	STATUS_STARTING JobStatus = "STARTING"
+	STATUS_STOPPING JobStatus = "STOPPING"
+	STATUS_FATAL    JobStatus = "FATAL"
+)
 
 func NewJob(config *ConfigProgram) *Job {
 	return &Job{
 		config:    config,
 		isRunning: false,
+		status:    STATUS_STOPPED,
 	}
 }
 
@@ -57,4 +52,22 @@ func (j *Job) Run() error {
 	}
 	err = j.cmd.Wait()
 	return err
+}
+
+// ParseCommand parses a command string into a path and arguments
+// Returns command path and arguments
+func ParseCommand(command string) (string, []string) {
+	var s scanner.Scanner
+	s.Init(strings.NewReader(command))
+	args := []string{}
+	token := s.Scan()
+	for token != scanner.EOF {
+		args = append(args, s.TokenText())
+		token = s.Scan()
+	}
+	path, err := exec.LookPath(args[0])
+	if err != nil {
+		path = args[0]
+	}
+	return path, args[1:]
 }
